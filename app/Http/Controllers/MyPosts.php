1e17519fac815e5213aabb3f3fd9dab2e6ce9adf
Mohamed Turki <mohamed.turki@areebgroup.com>
2022-08-08 13:39:01 +0200

    add some validation

--- app/Http/Controllers/MyPosts.php
+++ app/Http/Controllers/MyPosts.php
@@ -12,10 +12,29 @@ class MyPosts extends Controller
      public function createPost(Request $request)
      {
           $post = new Posts;
+          // $request->validate([
+          //      'title' => ['required', 'unique:posts', 'max:255'],
+          //      'description' => ['required']
+          // ]);
+
+          $this->validate(
+               $request,
+               [
+                    'title' => ['required', 'unique:posts', 'max:255'],
+                    'description' => ['required', 'max:600']
+               ],
+               [
+                    'required' => 'Why the :attribute field empty?',
+                    'title.unique' => 'Post title should be unique',
+                    'description.max' => 'Description cannot exceed 600 char'
+               ]
+          );
+
+
           $post->title = $request->input('title');
           $post->description = $request->input('description');
           $post->save();
-          return $post;
+          return response($post, 200);
      }
 
      public function getAllPosts()
@@ -33,14 +52,22 @@ class MyPosts extends Controller
      public function updatePost(Request $request, int $id)
      {
           $post = Posts::find($id);
+
+          $request->validate([
+               'title' => ['required', 'unique:posts', 'max:255'],
+               'description' => ['required']
+          ]);
+
           $post->title = $request->input('title');
           $post->description = $request->input('description');
-          return $post->update();
-          // abort(404);
+          $post->update();
+
+          return response($post, 200);
      }
 
      public function deletePost(int $id)
      {
-          return Posts::find($id)->delete();
+          Posts::find($id)->delete();
+          return response(['Message' => 'Post Deleted successfully']);
      }
 }
